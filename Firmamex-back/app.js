4a956698ebd4056e8a4d2193b1b00654573d879d
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2024-06-29 02:54:46 -0300

    Back done

--- Firmamex-back/app.js
+++ Firmamex-back/app.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './src/routes/index.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigins = ['http://localhost:5173', 'https://proyect-books.vercel.app'];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Si necesitas enviar cookies o credenciales
+}));
 app.use(express.json());
 app.use('/', router);
 
--- Firmamex-back/src/routes/index.js
+++ Firmamex-back/src/routes/index.js
@@ -1,24 +1,36 @@
-import {Router} from 'express';
-import axios from 'axios';
+import { Router } from "express";
+import axios from "axios";
 
 const router = Router();
 
+router.get("/books", (req, res) => {
+  let name = req.query.book;
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${name}&key=${process.env.APIKEY_BOOKS}`;
 
-router.get('/', (req, res) => {
-    res.send('Hello World !');
-});
-router.get('/books', (req, res) => {
-    let name = req.body.nombre;
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${name}&key=${process.env.APIKEY_BOOKS}`;
-
-    axios.get(url)
-        .then(response => {
-            res.json(response.data);
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json({ error: 'An error occurred' });
-        });
+  axios.get(url)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "An error occurred" });
+    });
 });
 
+router.post("/generate-story", async (req, res) => {
+  const { text1, text2 } = req.body;
+  try {
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.APIKEY_GEMINI}`;
+
+    const response = await axios.post(url,{
+        contents: [{parts: [{text: `Crea una nueva historia en español combinando los personajes principales de los siguientes libros en solo dos párrafos:\n\n**Texto 1:** ${text1}\n\n**Texto 2:** ${text2}\n\n**Historia combinada (dos párrafos)**:`, },],},],},
+      {headers: {"Content-Type": "application/json",},}
+    );
+    
+    let generatedText = response.data.candidates[0].content.parts[0].text;
+    res.json({ generatedText });
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+});
 export default router;
