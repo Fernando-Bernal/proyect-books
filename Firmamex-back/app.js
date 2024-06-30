5e2d26e2b5d1e456ce81c239899254f0346e0510
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2024-06-30 19:22:34 -0300

    fix cors

--- Firmamex-back/app.js
+++ Firmamex-back/app.js
@@ -7,19 +7,26 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const allowedOrigins = ['http://localhost:5173', 'https://proyect-books.vercel.app'];
+// const allowedOrigins = ['http://localhost:5173', 'https://proyect-books.vercel.app'];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true, // Si necesitas enviar cookies o credenciales
+// }));
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://proyect-books.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Si necesitas enviar cookies o credenciales
+  credentials: true
 }));
+
 app.use(express.json());
 app.use('/', router);
 
